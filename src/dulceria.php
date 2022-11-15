cc1bc65c74c5e909d461d7b568fd5fec5369fbb2
Francisco-Meza <L201140051@surnayarit.tecnm.mx>
2022-11-15 14:26:29 -0700

    Nav en dulceria

---
+++ src/dulceria.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+         <title>Dulceria</title>
+    </head>
+         
+    <body>
+        <?php
+            include('nav.html');
+        ?>
+        <div class="container">
+            <div class="imgFodo">
+                <div class="cabecera">
+                    <header class="header">
+                        <a href="index.html">
+                        <img src="assets/logoCM.png" alt="logo" class="logImg">
+                    </header>
+        <!-- Contenido principal -->
+            <main class="contenedor">
+                <h1 class="nuestrosProductos">Productos</h1>
+                <div class="productos">
+    
+                <!-- Inicio producto1 -->
+                    <div class="producto">
+                        <div class="fondoBlanco">
+                            <img src="assets/Mamut.png" alt="mamut">
+                        </div>
+                        <div class="infoProducto">
+                            <p class="prodNombre">MAMUT</p>
+                            <p class="prodPrecio">$10.00</p>
+                        </div>
+                    </div>
+                <!-- Inicio producto2 -->
+                 <div class="producto">
+                    <div class="fondoBlanco">
+                        <img src="assets/dove.png" alt="dove">
+                    </div>
+                    <div class="infoProducto">
+                        <p class="prodNombre">DOVE</p>
+                        <p class="prodPrecio">$18.00</p>
+                    </div>
+                 </div>
+                <!-- Inicio producto3 -->
+                  <div class="producto">
+                    <div class="fondoBlanco">
+                        <img src="assets/chispas.png" alt="chipsAhoy">
+                    </div>
+                    <div class="infoProducto">
+                        <p class="prodNombre">MILKA CHIPS AHOY</p>
+                        <p class="prodPrecio">$25.00</p>
+                    </div>
+                  </div>
+                <!-- Inicio producto4 -->
+                    <div class="producto">
+                       <div class="fondoBlanco">
+                            <img src="assets/ChocolateGalleta.png" alt="chocolateGalleta">
+                       </div>
+                      <div class="infoProducto">
+                            <p class="prodNombre">RITTER CHOCOLATE CON LECHE CON GALLETA</p>
+                            <p class="prodPrecio">$20.00</p>
+                       </div>
+                    </div>
+                <!-- Inicio producto5 -->
+                    <div class="producto">
+                        <div class="fondoBlanco">
+                            <img src="assets/ChocolateMazapan.png" alt="chocolateMazapan">
+                        </div>
+                        <div class="infoProducto">
+                            <p class="prodNombre">RITTER CHOCOLATE AMARGO CON MAZAPAN</p>
+                            <p class="prodPrecio">$29.00</p>
+                        </div>
+                    </div>
+                <!-- Inicio producto6 -->
+                    <div class="producto">
+                        <div class="fondoBlanco">
+                            <img src="assets/cremaMenta.png" alt="chocolateMenta">
+                        </div>
+                        <div class="infoProducto">
+                            <p class="prodNombre">RITTER CHOCOLATE AMARGO RELLENO CON CREMA DE MENTA</p>
+                            <p class="prodPrecio">$26.00</p>
+                        </div>
+                    </div>  
+                <!-- Inicio producto7 -->
+                    <div class="producto">
+                        <div class="fondoBlanco">
+                            <img src="assets/darkChocolate.png" alt="chocolateAmargo">
+                        </div>
+                        <div class="infoProducto">
+                            <p class="prodNombre">RITTER CHOCOLATE SEMIAMARGO 50% COCOA</p>
+                            <p class="prodPrecio">$30.00</p>
+                        </div>
+                    </div>
+                <!-- Inicio producto8 -->
+                    <div class="producto">
+                        <div class="fondoBlanco">
+                            <img src="assets/milkaOreo.png" alt="chocolateOreo">
+                        </div>
+                        <div class="infoProducto">
+                            <p class="prodNombre">MILKA OREO CHOCOLATE</p>
+                            <p class="prodPrecio">$25.00</p>
+                        </div>
+                    </div>
+                <!-- Inicio producto9 -->
+                    <div class="producto">
+                        <div class="fondoBlanco">
+                            <img src="assets/fruitAttack.png" alt="fruitAttack">
+                        </div>
+                        <div class="infoProducto">
+                            <p class="prodNombre">FRUIT ATTACK</p>
+                            <p class="prodPrecio">$20.00</p>
+                        </div>
+                    </div> 
+                <!-- Inicio producto10 -->
+                    <div class="producto">
+                        <div class="fondoBlanco">
+                            <img src="assets/whoppers.png" alt="whopperRobin">
+                        </div>
+                        <div class="infoProducto">
+                            <p class="prodNombre">WHOPPERS ROBIN EGGS</p>
+                            <p class="prodPrecio">$25.00</p>
+                        </div>
+                    </div> 
+                </main>                
+            </div>
+        </div>
+        </div>
+    </body>    
+
+</html>
