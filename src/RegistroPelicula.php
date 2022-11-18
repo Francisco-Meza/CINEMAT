34d421a74624909d2741e37cfb4667947ed6f05e
Francisco Meza <balazom2@gmail.com>
2022-11-17 21:53:52 -0700

    Validad js

--- src/RegistroPelicula.php
+++ src/RegistroPelicula.php
@@ -6,10 +6,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Registro de una pelicula</title>
         <link rel="stylesheet" href="css/style.css">
-        <Script>
-            nombre = document.getElementById("h22").style.border = "5px solid black";
-            alert("Esto es "+nombre);
-        </Script>
     </head>
     <body>
         <div class="container">
@@ -17,43 +13,53 @@
                 include("nav.html");
             ?>
                 <h2 id="h22">Registro de pelicula</h2>
-                <form class="form" action="">
-                        <div class="contenedor-form">
-                                <label for="" >Nombre</label>
-                                <br>
-                                <input type="text" name="Nombre" id="nombre" pattern="Nombre de la pelicula">
-                                <br>
-                                <label for="">Idioma</label>
-                                <br>
-                                <input type="text" name="Idioma" id="idioma" pattern="Idioma de la pelicula">
-                                <br>
-                                <label for="">Duraci&oacute;n</label>
-                                <br>
-                                <input type="text" name="Duración" id="duración" pattern="Nombre de la pelicula">
-                                <br>
-                                <label for="">Clasificaci&oacute;n </label>
-                                <br>
-                                <input type="text" name="Clasificacion" id="clasificacion" pattern="Clasificación de la pelicula">
-                                <br>
-                                <label for=""> G&eacute;nero </label>
-                                <br>
-                                <input type="text" name="Genero" id="genero" pattern="Género de la pelicula">
-                                <br>
-                                <label for="">Formato</label>
-                                <br>
-                                <input type="text" name="Formato" id="formato" pattern="Formato de la pelicula">
-                                <br>
-                                <label for="">Portada</label>
-                                <br>
-                                <input type="text" name="portada" id="Portada" pattern="Portada de la pelicula">
-                                <br>
-                                <label for="">Informaci&oacute;n </label>
-                                <br>
-                                <textarea  name="Información" id="información" pattern="Información de la pelicula"></textarea>
-                                <br>
-                            <button class="button-general" type="submit">Enviar</button>
-                        </div>
+                <form class="form" action="" onsubmit="return validar();">
+                    <label for="" >Nombre</label>
+                    <br>
+                    <input type="text" name="nombre" id="nombre" pattern="Nombre de la pelicula">
+                    <br>
+                    <label for="">Idioma</label>
+                    <br>
+                    <input type="text" name="Idioma" id="idioma" pattern="Idioma de la pelicula">
+                    <br>
+                    <label for="">Duraci&oacute;n</label>
+                    <br>
+                    <input type="text" name="Duración" id="duración" pattern="Nombre de la pelicula">
+                    <br>
+                    <label for="">Clasificaci&oacute;n </label>
+                    <br>
+                    <input type="text" name="Clasificacion" id="clasificacion" pattern="Clasificación de la pelicula">
+                    <br>
+                    <label for=""> G&eacute;nero </label>
+                    <br>
+                    <input type="text" name="Genero" id="genero" pattern="Género de la pelicula">
+                    <br>
+                    <label for="">Formato</label>
+                    <br>
+                    <input type="text" name="Formato" id="formato" pattern="Formato de la pelicula">
+                    <br>
+                    <label for="">Portada</label>
+                    <br>
+                    <input type="text" name="portada" id="Portada" pattern="Portada de la pelicula">
+                    <br>
+                    <label for="">Informaci&oacute;n </label>
+                    <br>
+                    <textarea  name="Información" id="información" pattern="Información de la pelicula"></textarea>
+                    <br>
+                    <button class="button-general" type="submit">Enviar</button>
                 </form>  
         </div>
+        <Script>
+            function validar(){
+                nombreInput = document.getElementById("nombre");
+                nombre = nombreInput.value;
+                if(nombre.length >10){
+                    nombreInput.value = 'Debe tener menos de 10 caracteres'
+                    nombreInput.focus();
+                    return false
+                }
+                document.form.submit();
+            }
+        </Script>
     </body>
 </html>
