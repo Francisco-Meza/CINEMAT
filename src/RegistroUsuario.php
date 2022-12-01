d6b50d06a0dcf118ba2f8c2cbd0d6995d1d42578
Francisco Meza <balazom2@gmail.com>
2022-11-30 19:59:59 -0700

    Quedo el js de los formularios

--- src/RegistroUsuario.php
+++ src/RegistroUsuario.php
@@ -97,59 +97,6 @@
                     </div>
                 </form>  
         </div>
-        <script>
-            const inputs = document.querySelectorAll('.formulario input');
-            const resultadoArray = [];
-            const expresiones = {
-                usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-                nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-                password: /^.{4,12}$/, // 4 a 12 digitos.
-                correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-                telefono: /^\d{7,14}$/, // 7 a 14 numeros.
-                numero: /^([0-9\:])*$/
-            }
-            function validar(){
-                for (let i = 0; i < inputs.length; i++) {
-                    resultado = false;
-                    password = "";
-                    switch(inputs[i].name){
-                        case "nombre":
-                            resultado = (expresiones.nombre.test(inputs[i].value))? true: false;
-                        break;
-                        case "apellido":
-                            resultado = (expresiones.nombre.test(inputs[i].value))? true: false;
-                        break;
-                        case "fecha_Nac":
-                            resultado = true;
-                        break;
-                        case "correo":
-                            resultado = (expresiones.correo.test(inputs[i].value))? true: false;
-                        break;
-                        case "password":
-                            resultado = (expresiones.password.test(inputs[i].value))? true: false;
-                            password = inputs[i].value;
-                        break;
-                        case "password2":
-                            resultado = (password == inputs[i].value && password!="")? true: false;
-                        break;
-                        default: resultado = true;
-                        break;
-                    }
-                    if (resultadoArray[i] == null) {
-                        if(!resultado){
-                            document.getElementById(`grupo__${inputs[i].name}`).classList.add('formulario__grupo-incorrecto');
-                            document.querySelector(`#grupo__${inputs[i].name} i`).classList.replace('formulario__error', 'formulario__error-activado');
-                            document.querySelector(`#grupo__${inputs[i].name} .formulario__grupo-error`).classList.replace('formulario__grupo-error','formulario__grupo-error-activado');
-                        }
-                    }else if(resultadoArray[i] != resultado){
-                        document.getElementById(`grupo__${inputs[i].name}`).classList.remove('formulario__grupo-incorrecto');
-                        document.querySelector(`#grupo__${inputs[i].name} i`).classList.replace('formulario__error-activado', 'formulario__error');
-                        document.querySelector(`#grupo__${inputs[i].name} .formulario__grupo-error-activado`).classList.replace('formulario__grupo-error-activado', 'formulario__grupo-error');
-                    }
-                    resultadoArray.push(resultado);
-                }
-                return false;
-            }
-        </script>
+        <script src="js/formulario.js"></script>
     </body>
 </html>
--- src/js/formulario.js
+++ src/js/formulario.js
@@ -1,102 +1,70 @@
-const formulario = document.getElementById("formulario");
-const inputs = document.querySelectorAll('#formulario input');
-
+const inputs = document.querySelectorAll('.formulario input');
+var mapResultado = new Map();
 const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
-	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-	password: /^.{4,12}$/, // 4 a 12 digitos.
-	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    password: /^.{4,12}$/, // 4 a 12 digitos.
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefono: /^\d{7,14}$/, // 7 a 14 numeros.
     numero: /^([0-9\:])*$/
 }
-const campos = {
-	usuario: false,
-	nombre: false,
-	password: false,
-	correo: false,
-	telefono: false,
-    apellido: false,
-    idioma: false,
-    duración: false,
-    clasificacion: false,
-    genero: false,
-    formato: false,
-    precio: false
-}
-const validarFormulario = (e) => {
-	switch (e.target.name) {
-		case "usuario":
-			validarCampo(expresiones.usuario, e.target, 'usuario');
-		break;
-		case "nombre":
-			validarCampo(expresiones.nombre, e.target, 'nombre');
-		break;
-        case "apellido":
-			validarCampo(expresiones.nombre, e.target, 'apellido');
-		break;
-		case "password":
-			validarCampo(expresiones.password, e.target, 'password');
-			validarPassword2();
-		break;
-		case "password2":
-			validarPassword2();
-		break;
-		case "correo":
-			validarCampo(expresiones.correo, e.target, 'correo');
-		break;
-		case "telefono":
-			validarCampo(expresiones.telefono, e.target, 'telefono');
-		break;
-        case "idioma":
-            validarCampo(expresiones.nombre, e.target, 'idioma');
-        break;
-        case "duración":
-            validarCampo(expresiones.numero, e.target, 'duración');
-        break;
-        case "clasificacion":
-            validarCampo(expresiones.nombre, e.target, 'clasificacion');
-        break;
-        case "genero":
-            validarCampo(expresiones.nombre, e.target, 'genero');
-        break;
-        case "formato":
-            validarCampo(expresiones.nombre, e.target, 'formato');
-        break;case "formato":
-            validarCampo(expresiones.nombre, e.target, 'formato');
-        break;
-        case "precio":
-            validarCampo(expresiones.numero, e.target, 'precio');
-        break;
-	}
-}
-const validarCampo = (expresion, input, campo) => {
-	if(expresion.test(input.value)){
-        document.getElementById(campo).style.border = '5px solid green';
-        campos[campo] = true;
-		/*document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos[campo] = true;*/
-	} else {
-        document.getElementById(campo).style.border = '5px solid RED';
-        campos[campo] = false;
-		/*document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos[campo] = false;*/
-	}
-}
-inputs.forEach((input) =>{
-    input.addEventListener('keyup', validarFormulario);
-	input.addEventListener('blur', validarFormulario);
-});
-formulario.addEventListener('submit',(e) =>{
-    e.preventDefault();
-    if(!campos.nombre){
-        alert("Revisa bien los campos llenados");
+error = 0;
+function validar() {
+    password = "";
+    resultado = false;
+    for (let i = 0; i < inputs.length; i++) {
+        switch (inputs[i].name) {
+            case "nombre":
+                resultado = (expresiones.nombre.test(inputs[i].value)) ? true : false;
+                break;
+            case "apellido":
+                resultado = (expresiones.nombre.test(inputs[i].value)) ? true : false;
+                break;
+            case "fecha_Nac":
+                resultado = true;
+                break;
+            case "correo":
+                resultado = (expresiones.correo.test(inputs[i].value)) ? true : false;
+                break;
+            case "password":
+                resultado = (expresiones.password.test(inputs[i].value)) ? true : false;
+                password = inputs[i].value;
+                break;
+            case "password2":
+                resultado = (password == inputs[i].value && password != "") ? true : false;
+                break;
+            default: resultado = true;
+                break;
+        }
+        if (mapResultado.size < 6) {
+            mapResultado.set(inputs[i].name, resultado);
+            if (!mapResultado.get(inputs[i].name)) {
+                incorrecto(inputs[i].name);
+                error++;
+            }
+        } else {
+            if (mapResultado.get(inputs[i].name) != resultado) {
+                if (resultado) {
+                    correcto(inputs[i].name);
+                    error--;
+                } else {
+                    incorrecto(inputs[i].name);
+                    error++;
+                }
+                mapResultado.set(inputs[i].name, resultado);
+            }
+        }
     }
-});
+    resultado = (error == 0) ? true:false;
+    return resultado;
+}
+function incorrecto(name) {
+    document.getElementById(`grupo__${name}`).classList.add('formulario__grupo-incorrecto');
+    document.querySelector(`#grupo__${name} i`).classList.replace('formulario__error', 'formulario__error-activado');
+    document.querySelector(`#grupo__${name} .formulario__grupo-error`).classList.replace('formulario__grupo-error', 'formulario__grupo-error-activado');
+}
+function correcto(name) {
+    document.getElementById(`grupo__${name}`).classList.remove('formulario__grupo-incorrecto');
+    document.querySelector(`#grupo__${name} i`).classList.replace('formulario__error-activado', 'formulario__error');
+    document.querySelector(`#grupo__${name} .formulario__grupo-error-activado`).classList.replace('formulario__grupo-error-activado', 'formulario__grupo-error');
+}
