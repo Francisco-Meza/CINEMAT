d2ca93f1b09837234b030a3b5fb257c31900fa7c
Francisco Meza <balazom2@gmail.com>
2022-12-09 07:39:37 -0700

    Subir

--- src/RegistroPelicula.php
+++ src/RegistroPelicula.php
@@ -21,8 +21,8 @@
                 </ul>
             </nav>
             <br>
-                <form  class="formulario" action="recibirPelicula.php" method="post">
-                    <dic class="formulario__h2">
+                <form enctype="multipart/form-data" class="formulario" action="recibirPelicula.php" method="post">
+                    <dic class="formulario__h2"> 
                         <h2>Registro de pelicula</h2>
                     </dic>
                     <!-------------------------NOMBRE----------------------->
@@ -42,9 +42,16 @@
                         <div class="formulario__grupo-input">
                            <!--<input type="text" class="formulario__input" name="idioma" id="idioma" placeholder="Idioma de la pelicula">--> 
                             <select class="formulario__input" name="idioma" id="">
-                                <option value="1">Primer Valor</option>
-                                <option value="2">Segundo Valor</option>
-                                <option value="3">Tercer Valor</option>
+                                <?php
+                                include_once("Connect.php");
+                                $consulta = "SELECT * FROM idioma";
+                                $ejecutar = mysqli_query($conn, $consulta)
+                                    or die("no se pudo leer los idiomas");
+
+                                foreach($ejecutar as $opciones){
+                                    echo "<option value='".$opciones['id']."'>".$opciones['nombre']."</option>";
+                                }
+                                ?>
                             </select>
                         </div>
                     </div>
@@ -65,9 +72,16 @@
                         <div class="formulario__grupo-input">
                            <!--<input type="text" class="formulario__input" name="clasificacion" id="clasificacion" placeholder="Clasificación de la pelicula">--> 
                             <select class="formulario__input" name="clasificacion" id="clasificacion">
-                                <option value="1">Primer Valor</option>
-                                <option value="2">Segundo Valor</option>
-                                <option value="3">Tercer Valor</option>
+                                <?php
+                                include_once("Connect.php");
+                                $consulta = "SELECT * FROM clasificacion";
+                                $ejecutar = mysqli_query($conn, $consulta)
+                                    or die("no se pudo leer las clasificaciones");
+
+                                foreach($ejecutar as $opciones){
+                                    echo "<option value='".$opciones['id']."'>".$opciones['nombre']."</option>";
+                                }
+                                ?>
                             </select>
                         </div>
                     </div>
@@ -77,30 +91,17 @@
                         <div class="formulario__grupo-input">
                             <!--<input type="text" class="formulario__input" name="genero" id="genero" placeholder="Género de la pelicula">-->
                             <select class="formulario__input" name="genero" id="genero">
-                                <option value="1">Primer Valor</option>
-                                <option value="2">Segundo Valor</option>
-                                <option value="3">Tercer Valor</option>
+                                <?php
+                                include_once("Connect.php");
+                                $consulta = "SELECT * FROM genero";
+                                $ejecutar = mysqli_query($conn, $consulta)
+                                    or die("no se pudo leer los generos");
+
+                                foreach($ejecutar as $opciones){
+                                    echo "<option value='".$opciones['id']."'>".$opciones['nombre']."</option>";
+                                }
+                                ?>
                             </select>
-                        </div>
-                    </div>
-                    <!-------------------------FORMATO----------------------->
-                    <div class="formulario__grupo" id="grupo__formato">
-                        <label for="nombre" class="formulario__label">Formato</label>
-                        <div class="formulario__grupo-input">
-                            <input type="text" class="formulario__input" name="formato" id="formato" placeholder="Formato de la pelicula">
-                            <i class="formulario__error fa-sharp fa-solid fa-circle-xmark"></i>
-                        </div>
-                        <div class="formulario__grupo-error">
-                            <p>El formato solo debe contener letras</p>
-                        </div>
-                    </div>
-                    <!-------------------------PORTADA----------------------->
-                    <div class="formulario__grupo"id="grupo__portada">
-                        <label for="portada" class="formulario__label">Portada</label>
-                        <div class="formulario__grupo-input">
-                            <div class="file__botton" >
-                                <input class="file__input-botton"type="file" name="portada" id="portada" placebolder="Portada">
-                            </div>
                         </div>
                     </div>
                     <!-------------------------INFORMACIÓN----------------------->
@@ -114,6 +115,16 @@
                             <p>la Informaci&oacute;n  solo debe contener letras</p>
                         </div>
                     </div>
+                    <!-------------------------PORTADA----------------------->
+                    <div class="formulario__grupo"id="grupo__portada">
+                        <label for="foto" class="formulario__label">Portada</label>
+                        <div class="formulario__grupo-input">
+                            <div class="file__botton" >
+                                <input class="file__input-botton" type="file" name="foto" id="portada">
+                            </div>
+                        </div>
+                    </div>
+                    <!----------------------------------------------------------------->
                     <div class="formulario__button">
                         <div class="formulario__button-mensaje">
                             <p>Porfavor llena todos los campos correctamente</p>
--- src/recibirPelicula.php
+++ src/recibirPelicula.php
@@ -5,19 +5,22 @@
     $genero=$_POST['genero'];
     $idioma=$_POST['idioma'];
     $clasificacion=$_POST['clasificacion'];
-    $formato=$_POST['formato'];
-    $portada=$_POST['portada'];
     $informacion=$_POST['informacion'];
-
-    echo "Los datos son los siguinetes: <br>";
-    echo "$nombre,$duracion,$genero,$idioma,$clasificacion,$formato,$portada,$informacion";
-  
-
-    $conectar=conn();
-    $sql="INSERT INTO pelicula(ID_PELICULA, NOMBRE, IDIOMA, DURACION,CLASIFICACION,GENERO,FORMATO,PORTADA,INFORMACION)
-     VALUES ('','$nombre','$idioma','$duracion','$clasificacion','$genero','$formato','$portada','$informacion')";
-     $resul = mysqli_query($conectar, $sql)or triger_error("Query Failed! SQL - Error: ".mysqli_error($conectar),E_USER_ERROR);
-
-     echo "$sql";
+    $foto = "";
+    if(isset($_FILES['foto']['name']))
+    {
+        $archivo_temporal=$_FILES['foto']['tmp_name'];
+        $f1 = fopen($archivo_temporal, "rb");
+        
+        $foto_size=$_FILES['foto']['size'];
+        $foto_recovertida = fread($f1, $foto_size);
+        
+        $foto_recovertida=addslashes($foto_recovertida);
+        $foto=$foto_recovertida;
+    }
+    $sql="INSERT INTO pelicula(NOMBRE, ID_IDIOMA, DURACION,ID_CLASIFICACION,ID_GENERERO,PORTADA,INFORMACION)
+     VALUES ('$nombre','$idioma','$duracion','$clasificacion','$genero','$foto','$informacion')";
+     $resul = mysqli_query($conn, $sql)or triger_error("Query Failed! SQL - Error: ".mysqli_error($conectar),E_USER_ERROR);
+    header("Location:index.php")
 ?>
  
