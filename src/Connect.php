9edf43f80749bb572312e73a3cb446d641914b8a
Francisco Meza <balazom2@gmail.com>
2022-12-10 13:46:45 -0700

    si

--- src/Connect.php
+++ src/Connect.php
@@ -1,7 +1,7 @@
 <?php
-	$servername = "localhost";
+	$servername = "189.182.55.213";
 	$database = "cinemat";
-	$username = "root";
+	$username = "equipo";
 	$password = "";
 	$conn = mysqli_connect($servername, $username, $password, $database)
 	 or die("Connection failed: " . mysqli_connect_error());
