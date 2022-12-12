f5dac3c48d5bb9b49f36ac136f32c4fc1c003d58
josue65390 <l201140013@surnayarit.tecnm.mx>
2022-12-12 04:22:07 -0700

    acercade

--- src/AcercaDe.php
+++ src/AcercaDe.php
@@ -7,7 +7,7 @@
     <body>
         <div class= "container">
             <?php
-                include("nav.html");
+                include("nav.php");
                 // session_start();
                 // if(!isset($_SESSION["correo"])){
                 //     header("Location: login.php");
